10bf393fbf2179e7d5992883fa90a372abd5b429
luisnava6667 <navaluisrodolfo@gmail.com>
2023-12-17 20:53:08 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<script setup>
+import { ref, computed, watch } from "vue"
+import Header from './components/Header.vue';
+import Button from "./components/Button.vue";
+import { calcularTotalPagar } from './helpers'
+const cantidad = ref(10000)
+const meses = ref(6)
+const total = ref(0)
+
+const min = 0
+const max = 20000
+const step = 100
+
+const formatearDinero = (valor) => {
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    })
+    return formatter.format(valor)
+}
+watch([cantidad, meses], () => {
+    total.value = calcularTotalPagar(cantidad.value, meses.value)
+})
+const pagoMensual = computed(()=>{
+    return total.value / meses.value
+})
+const handleChangeDecremento = () => {
+    const valor = cantidad.value - step
+    if (valor < min) {
+        alert('Cantidad no Valida')
+        return
+    }
+    cantidad.value = valor
+}
+const handleChangeIncremento = () => {
+    const valor = cantidad.value + step
+    if (valor > max) {
+        alert('Cantidad no Valida')
+        return
+    }
+    cantidad.value = valor
+}
+</script>
+
+<template>
+    <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
+        <Header />
+        <div class="flex justify-between mt-10">
+            <Button :operador="'-'" @fn="handleChangeDecremento" />
+            <Button :operador="'+'" @fn="handleChangeIncremento" />
+
+
+        </div>
+        <div class="my-5">
+            <input type="range" class="w-full bg-gray accent-lime-500 hover:accent-lime-600" :min="min" :max="max"
+                :step="step" v-model.number="cantidad">
+            <p class="text-center my-10 text-5xl font-extrabold text-indigo-600">{{ formatearDinero(cantidad) }}</p>
+            <!-- <p v-text="cantidad"></p> -->
+            <h2 class="text-2xl font-extrabold text-gray-500 text-center">
+                Elige un <span class="text-indigo-600"> Plazo </span>a pagar
+            </h2>
+            <select class="w-full bg-white border border-gray-300 rounded-lg text-center text-xl text-gray-500 mt-5"
+                :value="meses" v-model.number="meses">
+                <option value="6">6 Meses</option>
+                <option value="12">12 Meses</option>
+                <option value="24">24 Meses</option>
+            </select>
+        </div>
+        <div v-if="total > 0" class="my-5 space-y-3 bg-gray-50 p-5">
+            <h2 class="text-2xl font-extrabold text-gray-500 text-center">
+                Resumen <span class="text-indigo-600">de Pagos</span>
+            </h2>
+            <p class="text-xl text-gray-500 text-center font-bold">{{ meses }} Meses</p>
+            <p class="text-xl text-gray-500 text-center font-bold">Total a pagar: {{ formatearDinero(total) }}</p>
+            <p class="text-xl text-gray-500 text-center font-bold">Mensuales {{ formatearDinero(pagoMensual) }}</p>
+        </div>
+        <p v-else class="text-center">Añade una candidad y un plazo a pagar</p>
+    </div>
+</template>
+
+
